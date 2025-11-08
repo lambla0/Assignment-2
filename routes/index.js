9b6324d655f2d0856cd60000f033e874d25bb3bf
lambla0 <rajveer.singh4@ontariotechu.net>
2025-11-07 22:27:36 -0500

    Phase 4

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,18 @@ router.get('/projects', function(req, res) {
   res.render('projects', { title: "Projects" });
 });
 
+router.get('/project1', (req, res) => {
+  res.render('project1', { title: "Project One" });
+});
+
+router.get('/project2', (req, res) => {
+  res.render('project2', { title: "Project Two" });
+});
+
+router.get('/project3', (req, res) => {
+  res.render('project3', { title: "Project Three" });
+});
+
 // Contact page
 router.get('/contact', function(req, res) {
   res.render('contact', { title: "Contact Me" });
